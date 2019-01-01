6d99205a6291dd9427825705dec00dc31e502af7
CuiBeiJie <15655562705@163.com>
2019-01-01 14:37:42 +0800

    mybatis生成文件

---
+++ src/main/java/com/huawei/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.huawei.dao;
+
+import com.huawei.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ src/main/java/com/huawei/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.huawei.dao;
+
+import com.huawei.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
